28b3df15121c03bf627de9068fe7f1f9403d5fde
Aidan Gadberry <agadberr@gmail.com>
2016-11-05 12:33:31 -0700

    getStrChar and setStrChar added

--- String.cpp
+++ String.cpp
@@ -9,6 +9,28 @@ string String::getValue() {
 	return this->value;
 }
 
+void setStrChar(Variable index, Variable ch) {
+	int i = index->getValue();
+
+	if (i < this->value.length() && i >= 0) {
+		this->value[i] = ch->getValue();
+	} else {
+		printf("Error: index out of range - index %d in string %s\n",
+			   i, this->getValue());
+	}
+}
+
+void getStrChar(Variable index, Variable ch) {
+	int i = index->getValue();
+
+	if (i < this->value.length() && i >= 0) {
+		ch->setValue(this->value[i]);
+	} else {
+		printf("Error: index out of range - index %d in string %s\n",
+			   i, this->getValue());
+	}
+}
+
 // int main() {
 // 	String *str = new String();
 
--- String.h
+++ String.h
@@ -9,4 +9,6 @@ public:
 	String(string p_name = "", string p_value = "");
 	~String();
 	string getValue();
+	void setStrChar(Variable index, Variable ch);
+	void getStrChar(Variable index, Variable ch);
 };
