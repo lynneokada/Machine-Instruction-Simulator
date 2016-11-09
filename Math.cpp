d828b1c2714c1e70fa53f7af8bc5a44074f78836
lokada <lyokada@ucsc.edu>
2016-11-09 03:21:06 -0800

    setValue changed

--- Math.cpp
+++ Math.cpp
@@ -75,7 +75,14 @@ double Math::getValue()
 
 void Math::setValue(std::vector<string> names, std::map<string, Math*> map)
 {	
-	// value = map[names[0]]->getValue();
+	double val = mathVariables[v_line[i][1]]->getValue();
+    if(mathVariables[v_line[i][1]]->getType() == "Real") {
+        mathVariables.insert(pair<string,Real*>(name, new Real(name, real_value)));
+    } else if(mathVariables[v_line[i][1]->getType() == "Numeric"]) {
+    	mathVariables.insert(pair<string,Real*>(name, new Numeric(name, real_value)));
+    }
+    mathVariables->erase(v_line[i][1]);
+
 	value = 6;
 }
 
