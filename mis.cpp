7f8b290aefddf9767fe1dd38df03ce1919a04bfc
Travis Takai <travistakai@gmail.com>
2016-11-09 10:37:52 -0800

    Fixed jump conditions/checking.

--- mis.cpp
+++ mis.cpp
@@ -157,6 +157,13 @@ vector<string> Mis::obtain_args(int index, vector<string> v_single_line) {
 		{
 			continue;
 		}
+
+		if(j == 1 && v_single_line[0].find("JMP") != string::npos) {
+			params.push_back(v_single_line[1]);
+			cout << v_single_line[1] <<endl;
+			continue;
+		}
+
 		string paramName = v_single_line[j];
 		cout << "Line contains: " << paramName << endl;
 
@@ -166,6 +173,7 @@ vector<string> Mis::obtain_args(int index, vector<string> v_single_line) {
 			params.push_back(paramName);
 		} 
 		else if (v_single_line[j].find_first_of("'") != string::npos && v_single_line[j].size() == 3) { //check for char
+			cout << "Char args" << endl;
 			std::regex rgx("('[^\"]')");
 			auto begin = std::sregex_iterator(v_single_line[j].begin(), v_single_line[j].end(), rgx);
 			auto end = std::sregex_iterator();
@@ -176,7 +184,6 @@ vector<string> Mis::obtain_args(int index, vector<string> v_single_line) {
 			params.push_back(paramName);
 
 		} else if(v_single_line[j].find_first_of("\"") != string::npos) { //check for string
-			cout << v_single_line[j] << "test";
 			std::regex rgx("(\"[^\"]*\")");
 			auto begin = std::sregex_iterator(v_single_line[j].begin(), v_single_line[j].end(), rgx);
 			auto end = std::sregex_iterator();
@@ -309,9 +316,10 @@ int main(int argc, char *argv[])
 		} else if (v_line[i][0].find("JMP") != string::npos) {
 			vector<string> params = mis.obtain_args(i,v_line[i]);
 			int labelIndex = mis.jmp.compare(params, v_line[i][0], mathVariables);
-			cout << "current index " << i << " : jmp index " << labelIndex << endl;
-			if (labelIndex < 0 || labelIndex > v_line.size()) {
+			if(labelIndex == -2) {
 				exit(EXIT_FAILURE);
+			} else if (labelIndex == -1){
+				continue;
 			} else {
 				i = labelIndex-1;
 			}
